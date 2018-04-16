bf09f56f2740e35665487b89d3b36e3a1f8deaad
Ulrik <ulrik.andersson@gmail.com>
2018-04-16 15:46:38 +0200

    Create script.js

---
+++ statistics/script.js
@@ -0,0 +1,11 @@
+<!-- Stats -->
+<script type="text/javascript">
+    var _btstats = _btstats || {};
+    _btstats.id = {BUSINESS_ID};
+    _btstats.channel = {CHANNEL_ID};
+    (function () {
+        var btscript = document.createElement('script'); btscript.type = 'text/javascript'; btscript.async = true;
+        btscript.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.basetool.se/script.js';
+        var el = document.getElementsByTagName('script')[0]; el.parentNode.insertBefore(btscript, el);
+    })();
+</script>
